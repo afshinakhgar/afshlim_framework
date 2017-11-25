e63d6a2b3f1a6e923658783882c23274a39cfa2a
Afshin Akhgar <afshin.akhgar@gmail.com>
2017-11-25 11:45:04 +0330

    travis cli added

--- tests/app/UserTest.php
+++ tests/app/UserTest.php
@@ -9,11 +9,7 @@ use Core\Interfaces\_AppTest;
 class UserTest extends _AppTest
 {
 
-    public function testGetUser()
-    {
-        $user = UserDataAccess::getById(1);
-        $this->assertEquals($user->id,1);
-    }
+
 
     public function testSample() {
         $this->assertTrue(true);
