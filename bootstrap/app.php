6682394176b1d2b34cf71e2a9479eb0d2349d4c3
Afshin <afshin.akhgar@gmal.com>
2017-11-28 00:35:57 +0330

    php errors on debug mode

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,15 @@ require  __APP_ROOT__.'bootstrap/bootstrap.php';
 require  __APP_ROOT__.'core/Functions/helpers.php';
 require __APP_ROOT__.'vendor/autoload.php';
 require __APP_ROOT__.'config/settings.php';
+
+
+if($settings['debug'] == true) {
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 $app = new \Core\App($config);
 
 use SlimFacades\Facade;
