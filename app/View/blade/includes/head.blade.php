56c5e088e535c39f382d94fa1925fc6ed6635b81
Afshin Akhgar <afshin.akhgar@gmail.com>
2017-12-02 18:03:06 +0330

    some changes in bootstrap css

--- app/View/blade/includes/head.blade.php
+++ app/View/blade/includes/head.blade.php
@@ -1,10 +1,7 @@
 <meta charset="UTF-8">
 <title>{{ $title or '' }}</title>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<style>
-    body {
-        min-height: 2000px;
-        padding-top: 70px;
-    }
-</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
--- app/View/blade/includes/header.blade.php
+++ app/View/blade/includes/header.blade.php
@@ -1,3 +1,20 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">Logo</a>
+
+    <!-- Links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 2</a>
+        </li>
+
+
+    </ul>
+</nav>
+<br>
 <div class="container">
 
     <!-- Main component for a primary marketing message or call to action -->
